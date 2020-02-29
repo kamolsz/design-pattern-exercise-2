b35df0c088d144c3e697125327ccbd2e19423595
Kamil Olszewski <kamil.j.olszewski@gmail.com>
2020-02-29 12:40:50 +0100

    Logger iplementation

--- src/main/java/account/AccountService.java
+++ src/main/java/account/AccountService.java
@@ -3,14 +3,17 @@ package account;
 public class AccountService {
 
     public void withdraw(String accountNumber, double amount) {
-        if(amount>0 && AccountNumberValidator.validate(accountNumber)) {
-            System.out.println(amount + " has been withdraw from " + accountNumber);
+        if (amount > 0 && AccountNumberValidator.validate(accountNumber)) {
+//            System.out.println(amount + " has been withdraw from " + accountNumber);
+            Logger.getInstance().log(amount + " has been withdraw from " + accountNumber);
         }
     }
 
     public void deposit(String accountNumber, double amount) {
-        if(amount>0 && AccountNumberValidator.validate(accountNumber)) {
-            System.out.println(amount + " has been deposit into " + accountNumber);
+        if (amount > 0 && AccountNumberValidator.validate(accountNumber)) {
+//            System.out.println(amount + " has been deposit into " + accountNumber);
+            Logger.getInstance().log(amount + " has been deposit into " + accountNumber);
+
         }
     }
 }
---
+++ src/main/java/account/Logger.java
@@ -0,0 +1,25 @@
+package account;
+
+public class Logger {
+
+    private static Logger instance;
+
+    private Logger() {
+    }
+
+    public static Logger getInstance() {
+        if (instance == null) {
+            synchronized (Logger.class) {
+                if (instance == null) {
+                    instance = new Logger();
+                }
+            }
+        }
+        return instance;
+    }
+
+    public void log(String name) {
+        System.out.println(name);
+
+    }
+}
